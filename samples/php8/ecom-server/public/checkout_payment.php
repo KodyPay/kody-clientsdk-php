eefc4e9da2e073fe56d51ed8f33cee994cd21dcd
Ioannis Petridis <ioannis.petridis@kody.com>
2025-04-14 12:24:17 +0100

    Interim

--- samples/php8/ecom-server/public/checkout_payment.php
+++ samples/php8/ecom-server/public/checkout_payment.php
@@ -4,6 +4,7 @@ $config = require __DIR__ . '/config.php';
 
 use Com\Kodypay\Grpc\Ecom\V1\KodyEcomPaymentsServiceClient;
 use Com\Kodypay\Grpc\Ecom\V1\PaymentInitiationRequest;
+use Com\Kodypay\Grpc\Ecom\V1\PaymentStatus;
 use Grpc\ChannelCredentials;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -19,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $request->setAmountMinorUnits($_POST['amount']);
     $request->setCurrency($_POST['currency']);
     $request->setOrderId($_POST['order_id']);
-    $request->setReturnUrl($config['redirect_url'].'?paymentReference='.$paymentReference);
+    $request->setReturnUrl($config['redirect_url'] . '?paymentReference=' . $paymentReference);
 
     if (isset($_POST['enable_expiration'])) {
         $expiry = new PaymentInitiationRequest\ExpirySettings();
@@ -30,23 +31,46 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $request->setExpiry($expiry);
     }
 
-    list($response, $status) = $client->InitiatePayment($request, $metadata)->wait();
+    $timeoutDateTime = (new DateTime())->add(new DateInterval('PT' . (3 * 60) . 'S'));
 
-    if ($status->code === \Grpc\STATUS_OK) {
-        if ($response->hasResponse()) {
-            $paymentUrl = $response->getResponse()->getPaymentUrl();
-            header('Location: ' . $paymentUrl);
-            exit;
-        } else {
-            $error = $response->getError()->getMessage();
-            header('Location: checkout.php?error=' . urlencode($error));
-            exit;
+    $responses = $client->InitiatePaymentStream($request, $metadata, ['timeout' => $timeoutDateTime]) -> responses();
+
+    foreach ($responses as $reply) {
+        if ($reply->getResponse()) {
+            $status=$reply->getResponse()->getStatus().PHP_EOL;
+            if($status != PaymentStatus::PENDING){
+                break;
+            }
+            $paymentId = $reply->getResponse()->getPaymentId().PHP_EOL;;
+            $paymentUrl = $reply->getResponse()->getPaymentData()->getPaymentWallet()->getPaymentLinkId().PHP_EOL;;
+            $status=$reply->getResponse()->getStatus().PHP_EOL;
+            echo "<h2>Collecting payment for Payment ID: $paymentId</h2>";
+            echo "<h2>Payment status:$status</h2>";
+            echo "<h2>Payment Url: $paymentUrl</h2>";
+        } else{
+            error_log("Error: Unable to initiate payment.");
+            echo "<h2>Error: Unable to initiate payment.</h2>";
+            break;
         }
-    } else {
-        $error = 'gRPC error: ' . $status->details;
-        header('Location: checkout.php?error=' . urlencode($error));
-        exit;
     }
+
+//    if ($paymentId && $paymentStatus == PaymentStatus::PENDING) {
+//        echo "<h2>Collecting payment for Payment ID: $paymentId</h2>";
+//        echo "<h2>Payment Url: $paymentUrl</h2>";
+//        echo "<div id='loading'>Waiting for payment...</div>";
+//        echo "<input type='hidden' name='store_id' value='" . htmlspecialchars($config['store_id']) . "'>";
+//        echo "<input type='hidden' name='amount' value='" . htmlspecialchars($_POST['amount']) . "'>";
+//        echo "<input type='hidden' name='payment_url' value='" . htmlspecialchars($paymentUrl) . "'>";
+//        echo "</div>";
+//    } elseif ($paymentStatus != PaymentStatus::PENDING){
+//            error_log("Payment something");
+//            header('Location: checkout.php');
+//            exit;
+//    }
+//    else {
+//        error_log("Error: Unable to initiate payment.");
+//        echo "<h2>Error: Unable to initiate payment.</h2>";
+//    }
 } else {
     header('Location: checkout.php');
     exit;

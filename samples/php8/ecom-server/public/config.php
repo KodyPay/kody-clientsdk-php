38ff20abf43f57d578207179174fb003a50cf1b6
kf <7kfpun@users.noreply.github.com>
2025-04-14 17:58:56 +0800

    upt: use default if the env is not set (#46)

--- samples/php8/ecom-server/public/config.php
+++ samples/php8/ecom-server/public/config.php
@@ -24,8 +24,9 @@ $config = [
     'store_id' => $storeIdFromCookie ?: $_ENV['KODY_STORE_ID'] ?: '',
     'api_key' => $apiKeyFromCookie ?: $_ENV['KODY_API_KEY'] ?: '',
     'currency' => $_ENV['KODY_STORE_CURRENCY'] ?: '',
-    'currencies' => $_ENV['KODY_STORE_CURRENCIES']
-        ? explode(',', $_ENV['KODY_STORE_CURRENCIES']) : ["GBP", "HKD", "USD"],
+    'currencies' => (isset($_ENV['KODY_STORE_CURRENCIES']) && $_ENV['KODY_STORE_CURRENCIES'])
+        ? explode(',', $_ENV['KODY_STORE_CURRENCIES'])
+        : ["GBP", "HKD", "USD"],
     'redirect_url' => $_ENV['PAYMENT_REDIRECT_URL'] ?: '',
     'expiring_seconds' => $_ENV['PAYMENT_EXPIRING_SECONDS'] ?: 900,
 ];
